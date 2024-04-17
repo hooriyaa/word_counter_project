ecd0da4c4c2b38cc52109f6bb38c5731facfe19b
hooriya <parih3080@gmail.com>
2024-04-17 19:27:35 +0500

    push my word counter project

---
+++ bin/index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node 
+// This line is called a shebang. it tells the OS to run it with nodejs.
+// import the "inquirer" module, which is a commmand line interface for node.js 
+import inquirer from "inquirer";
+const input = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter you sentence to count the words!!!:"
+    }
+]);
+const words = input.Sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence words are ${words.length}`);
